29009cdabf3490f1fbb3bdb245807d19a68be8db
okcrick <mmccbijepur11@gmail.com>
2025-10-28 17:30:05 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
+  webpack: (config, { isServer }) => {
+    // avoid bundling 'undici' into the server bundle (prevents parse errors)
+    if (isServer) {
+      // ensure externals exists and push undici
+      if (!config.externals) config.externals = [];
+      config.externals.push("undici");
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
-
